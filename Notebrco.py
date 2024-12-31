f2a6a2a13e35437bff34a46fa24c4aa45e0a014e
Mibish <mibishghimire798@gmail.com>
2024-12-31 11:59:13 +0545

    mibish

---
+++ Notebrco.py
@@ -0,0 +1,13 @@
+'''library=['react','python','java']
+for i in library:
+    if i=='python':
+        print("book found")
+        break
+print(i)'''
+
+potato=[1,2,3,4,5]
+for i in potato:
+    if i==3:
+        continue
+    print(i)
+else: print('gg')
